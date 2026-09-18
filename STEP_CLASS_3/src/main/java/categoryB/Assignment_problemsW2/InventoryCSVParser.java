40a37644ec67b7b4cc138cc640da59f22015ccf6
mahati21 <mahatigr21@gmail.com>
2026-09-19 00:14:11 +0530

    Week 2 : Java problems are implemented

---
+++ STEP_CLASS_3/src/main/java/categoryB/Assignment_problemsW2/InventoryCSVParser.java
@@ -0,0 +1,17 @@
+package categoryB.Assignment_problemsW2;
+
+public class InventoryCSVParser {
+    static void parseInventoryRecord(String csvLine) {
+        String[] fields = csvLine.split(",");
+        if (fields.length != 3)
+        {
+            System.out.println("Invalid Record");
+            return;
+        }
+        System.out.println("Product: " + fields[0] + " | SKU: " + fields[1] + " | Qty: " + fields[2]);
+    }
+    public static void main(String[] args)
+    {
+        parseInventoryRecord("Wireless Mouse,WM-2201,150");
+    }
+}
